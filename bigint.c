aaddcdc3a6f9868ce04e1d3548f7d0fa3eae57e1
bernardo1r <bernardo2010m@hotmail.com>
2019-09-29 00:26:28 -0300

    corrigida funcao de soma e impletada a funcao de multiplicacao

--- bigint.c
+++ bigint.c
@@ -19,6 +19,18 @@ void big_val (BigInt res, long val)
         
     }
 }
+
+/* Funcão Auxiliar */
+
+int big_isZero(BigInt a) {
+    int i;
+    for (i=0; i < (NUM_BITS/8) - 1; i++) {
+        if (a[i] != 0)
+            return 0;
+    }
+    return 1;
+}
+
 /* Operações Aritméticas */
 
 /* res = -a */
@@ -42,6 +54,8 @@ void big_sum(BigInt res, BigInt a, BigInt b)
     for(i=0; i < (NUM_BITS/8); i++) {
         if ((a[i]+b[i] > UCHAR_MAX))
             curr = 1;
+        else
+            curr = 0;
         res[i] = a[i]+b[i]+old;
         old = curr;
     }
@@ -58,40 +72,56 @@ void big_sub(BigInt res, BigInt a, BigInt b)
 /* res = a * b */
 void big_mul(BigInt res, BigInt a, BigInt b)
 {
-
-}
+    big_val(res, 0);  // initialize result
+    int sig = 0;
+    if ((a[(NUM_BITS/8)-1] >> 7)&0x1){
+        sig = 1;
+        big_comp2(a, a);
+    }
+    if ((b[(NUM_BITS/8)-1] >> 7)&0x1){
+        sig ^= 1;
+        big_comp2(b, b);
+    } 
+    while (!big_isZero(b)) 
+    { 
+        // If second number becomes odd, add the first number to result 
+        if (b[0]&0x1) 
+            big_sum(res, res, a); 
+        // Double the first number and halve the second number 
+        big_shl(a, a, 1); 
+        big_shr(b, b, 1);
+     }
+     if(sig)
+        big_comp2(res, res); 
+}   
 
 /* Operações de Deslocamento */
 
 /* res = a << n */
 void big_shl(BigInt res, BigInt a, int n)
 {   
-    unsigned char msb1;
-    unsigned char msb2;
-
-    for(int i = 0; i < NUM_BITS/8; i ++) //Setting up res
-    {
-        res[i] = a[i];
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        msb1 = res[i/8] >> 7; //Getting the most significant bit
-
-        res[i/8] = res[i/8] << 1;
-        
-        for (int j = i/8 + 1; j < NUM_BITS/8; j++) //Shifting the other elements 
-        {   
-            msb2 = res[j] >> 7; //Getting the most significant bit
-
-            res[j] = res[j] << 1;
-
-            res[j] |= msb1; //Setting the least significant bit to msb1
-
-            msb1 = msb2;
+    int index = ((NUM_BITS-1) - n) / 8;
+    int bit = ((NUM_BITS-1) - n) % 8;
+    int indexRes = NUM_BITS/8;
+    int bitRes = 3;
+    int i;
+    for (i=0; i < NUM_BITS/8; i++)
+        res[i] = 0;
+    if (n >= 128)
+        return;
+    while(index >= 0) {
+        while(bit >= 0) {
+            if (bitRes < 0) {
+                indexRes--;
+                bitRes = 3;
+            }
+            res[indexRes] |= a[index] << (bitRes - bit);
+            bit--;
         }
+        index--;
+        bit = 3;
     }
-    
+
 }
 
 /* res = a >> n (lógico)*/
