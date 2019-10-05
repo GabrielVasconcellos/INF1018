5d91ef5458679fb20de8861f7e42d2cbc7e1ffbf
bernardo <bernardo2010m@hotmail.com>
2019-10-05 17:26:02 -0300

    adicionado comentarios nas funcoes de soma e multiplicacao

--- bigint.c
+++ bigint.c
@@ -29,19 +29,19 @@ void big_copy(BigInt res, BigInt a) {
 void big_val (BigInt res, long val)
 {
     int i;
-    int sig = (val >> ((sizeof(val)*8)-1)) & 0x1; //pega o bit mais significativo de val
+    int sig = (val >> ((sizeof(val)*8)-1)) & 0x1; /* pega o bit mais significativo de val */
     unsigned char *walker = (unsigned char*) & val;
 
     for (i = 0;  i < (NUM_BITS/8); i++)
     {
-        if (i < sizeof(val)) //coloca o valor de val em res
+        if (i < sizeof(val)) /* coloca o valor de val em res */
         { 
             res[i] = *walker;
             walker++;
         }
-        else if(sig) //se o numero for negativo completa os bits do vetor com 1
+        else if(sig) /* se o numero for negativo completa os bits do vetor com 1 */
             res[i] = 0xff;
-        else //se o numero for positivo completa os bits do vetor com 0
+        else /* se o numero for positivo completa os bits do vetor com 0 */
             res[i] = 0;
     }
 }
@@ -51,14 +51,15 @@ void big_val (BigInt res, long val)
 /* res = -a */
 void big_comp2(BigInt res, BigInt a)
 {
-    int i, diff = 1;
+    int i;
+    int carry = 1; /* inicializa o carry com o numero 1 para o complemento a dois */
 
     for (i=0; i < (NUM_BITS/8); i++) {
         res[i] = ~a[i];
-        if(diff) {
-            diff += res[i];
-            res[i] = diff & 0xff;
-            diff >>= 8;
+        if(carry) { /* enquanto houver carry */ 
+            carry += res[i];
+            res[i] = carry & 0xff; /* coloca em res apenas os 8 primeiros bits de diff */
+            carry >>= 8; /* retira os 8 primeiros bits do carry */
         }
     }
 }
@@ -66,13 +67,13 @@ void big_comp2(BigInt res, BigInt a)
 /* res = a + b */
 void big_sum(BigInt res, BigInt a, BigInt b)
 {
-    unsigned int curr=0;
+    unsigned int carry=0; /* inicializa o carry com zero já que não possui resto */
 
     for(int i = 0; i < (NUM_BITS/8); i++)
     {
-        curr += a[i]+b[i];
-        res[i] = curr & 0xff;
-        curr >>= 8;
+        carry += a[i]+b[i];
+        res[i] = carry & 0xff; /* coloca em res os 8 primeiros bits de diff */
+        carry >>= 8; /* retira os 8 primeiros bits do carry */
     }
 }
 
@@ -88,33 +89,40 @@ void big_sub(BigInt res, BigInt a, BigInt b)
 /* res = a * b */
 void big_mul(BigInt res, BigInt a, BigInt b)
 {
-    BigInt funcA;
-    BigInt funcB;
+    BigInt aAux;
+    BigInt bAux;
     int sig = 0;
 
-    big_copy(funcA, a);
-    big_copy(funcB, b);
+
+    /* coloca os BigInts a e b em variáveis auxiliares */
+    big_copy(aAux, a);
+    big_copy(bAux, b);
+
     big_val(res, 0);  
     
-    if ((funcA[(NUM_BITS/8)-1] >> 7) & 0x1)
+    /* Verifica se a e b são negativos */
+    if ((aAux[(NUM_BITS/8)-1] >> 7) & 0x1)
     {
         sig = 1;
-        big_comp2(funcA, funcA);
+        big_comp2(aAux, aAux);
     }
-    if ((funcB[(NUM_BITS/8)-1] >> 7) & 0x1)
+    if ((bAux[(NUM_BITS/8)-1] >> 7) & 0x1)
     {
         sig ^= 1;
-        big_comp2(funcB, funcB);
+        big_comp2(bAux, bAux);
     } 
-    while (!big_isZero(funcB)) 
+
+    /* Faz a multiplicação de numeros positivos */
+    while (!big_isZero(bAux)) /* Enquanto b for diferente de zero */
     { 
-        if (funcB[0] & 0x1) 
-            big_sum(res, res, funcA); 
+        if (bAux[0] & 0x1) /* se b for ímpar */
+            big_sum(res, res, aAux); 
         
-        big_shl(funcA, funcA, 1); 
-        big_shr(funcB, funcB, 1);
+        /* Dobra o valor de a e faz a metade de b */
+        big_shl(aAux, aAux, 1); 
+        big_shr(bAux, bAux, 1);
     }
-    if(sig)
+    if(sig) /* se apenas um dos numeros for negativo o resultado da multiplição também será */
         big_comp2(res, res); 
 }   
 
@@ -132,14 +140,14 @@ void big_shl(BigInt res, BigInt a, int n)
 
     for (int i = 0; i < n; i++)
     {      
-        msb1 = res[i/8] >> 7; //Armazenando o bit mais significativo
+        msb1 = res[i/8] >> 7; /* Armazenando o bit mais significativo */
         res[i/8] = res[i/8] << 1;
 
-        for (int j = i/8 + 1; j < NUM_BITS/8; j++) //Deslocando os outros elementos
+        for (int j = i/8 + 1; j < NUM_BITS/8; j++) /* Deslocando os outros elementos */
         {   
-            msb2 = res[j] >> 7; //Armazenando o bit mais significativo
+            msb2 = res[j] >> 7; /* Armazenando o bit mais significativo */
             res[j] = res[j] << 1;
-            res[j] |= msb1; //Trocando o bit menos significativo por msb1
+            res[j] |= msb1; /* Trocando o bit menos significativo por msb1 */
             msb1 = msb2;
         }
     }
@@ -161,9 +169,9 @@ void big_shr(BigInt res, BigInt a, int n)
 
         for (int j = ((NUM_BITS - 8 )/8)-i/8; j >= 0; j--)
         {   
-            lsb2 = res[j] << 7; //Armazenando o bit menos significativo
+            lsb2 = res[j] << 7; /* Armazenando o bit menos significativo */
             res[j] = res[j] >> 1;
-            res[j] |= lsb1; //Trocando o bit mais significativo por lsb1
+            res[j] |= lsb1; /* Trocando o bit mais significativo por lsb1 */
             lsb1 = lsb2;   
         }
     }
@@ -185,13 +193,13 @@ void big_sar(BigInt res, BigInt a, int n)
         
         for (int j = (NUM_BITS  -8 -i)/8; j >= 0; j--)
         {   
-            lsb2 = res[j] << 7; //Armazenando o bit menos significativo
+            lsb2 = res[j] << 7; /* Armazenando o bit menos significativo */
             if (j == (NUM_BITS  -8 -i)/8)
                 res[j] = (signed char) res[j] >> 1;
             else
                 res[j] >>= 1; 
 
-            res[j] |= lsb1; //Trocando o bit mais significativo por lsb1
+            res[j] |= lsb1; /* Trocando o bit mais significativo por lsb1 */
             lsb1 = lsb2;
         }
     }
