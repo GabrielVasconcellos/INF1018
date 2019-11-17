66b70a932171438468492aebcd02f389ef3e25d8
Aluno DI-PUC-Rio <aluno@localhost.localdomain>
2019-11-16 22:19:00 -0200

    Comecando o trabalho + enums + PARAM

--- CriaFunc/cria_func.c
+++ CriaFunc/cria_func.c
@@ -1,15 +1,114 @@
 #include "cria_func.h"
 
+typedef enum {RDI, RSI, RDX} reg;
+
+unsigned char* insertCode(unsigned char *codigo, char *s)
+{	
+	for(; *(s-1); s += 2)
+	{
+		*codigo = 0x0;
+		for(int j = 0; j < 2; j++)
+		{
+			if(s[j] <= '9')
+				*codigo = *codigo * 16 + (s[j] - '0');
+			else
+				*codigo = *codigo * 16 + (s[j] - 'a' + 10);
+		}
+		codigo++;
+	}
+	return codigo;
+}
+
 void* cria_func (void* f, DescParam params[], int n)
 {
-    for(int i = 0; i > n; i++)
+	unsigned char *codigo = malloc(sizeof(unsigned char) * 100);
+	unsigned char *start = codigo;
+	reg arg = RDI;
+	reg f_arg = RDI;
+	
+	codigo = insertCode(codigo, "554889e54883ec2048897c24e048897424e848895424f0");
+	
+    for(int i = 0; i < n; i++, arg++)
     {
-        switch (params[i].tipo_val)
+        switch (params[i].orig_val)
         {
-            case INT_PAR:
-                /* code */
+            case PARAM:
+                switch(params[i].tipo_val)
+				{
+					case INT_PAR:
+                		switch(f_arg)
+                		{
+                			case RDI:
+                				switch(arg)
+                				{
+                					case RSI:
+                						codigo = insertCode(codigo, "8b7424e0");
+                						break;
+                					case RDX:
+                						codigo = insertCode(codigo, "8b5424e0");
+                						break;
+                				}
+                				break;
+                			case RSI:
+                				switch(arg)
+                				{
+                					case RDX:
+                						codigo = insertCode(codigo, "8b5424e8");
+                						break;
+                				}
+                				break;
+                			case RDX:
+                				break;
+                		}
+                		break;
+					case PRT_PAR:
+						switch(f_arg)
+		            		{
+		            			case RDI:
+		            				switch(arg)
+		            				{
+		            					case RSI:
+		            						codigo = insertCode(codigo, "488b7424e0");
+		            						break;
+		            					case RDX:
+		            						codigo = insertCode(codigo, "488b5424e0");
+		            						break;
+		            				}
+		            				break;
+		            			case RSI:
+		            				switch(arg)
+		            				{
+		            					case RDX:
+		            						codigo = insertCode(codigo, "488b5424e8");
+		            						break;
+		            				}
+		            				break;
+		            			case RDX:
+		            				break;
+		            	}
+		            	break;
+				}
+				f_arg++;
                 break;
-            case PTR_PAR:
+            case FIX:
+                switch(params[i].tipo_val)
+                {
+                	case INT_PAR:
+                		switch(arg)
+                		{
+                			case RDI:
+                				break;
+                			case RSI:
+                				break;
+                			case RDX:
+                				break;
+                		}
+                		break;
+                	case PTR_PAR:
+                		break;
+                }
+                break;
+			case IND:
                 /* code */
                 break;
         }
